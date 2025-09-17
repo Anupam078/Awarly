707e506ad7970510f921d7e14924c2edd4d86061
Anupam Ojha <anupamojha078@gmail.com>
2025-09-18 02:19:12 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 function App() {
-  // The <Outlet> component renders the active route's element.
-  // e.g., on "/", it renders <RoleSelection />, on "/dashboard", it renders <Dashboard />
   return (
     <div className="App">
       <Outlet />
